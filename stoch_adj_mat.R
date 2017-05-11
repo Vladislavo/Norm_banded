793e1dfe768ce951fab009072923cf7bbdb335fe
Vladi <rykovinternational@gmail.com>
2017-05-11 17:58:17 +0200

    Function that computes stochastic adjacency matrix used in the PageRank algorithm by Google and evaluates nodes of the adjacent graph accorings to their connectivity

---
+++ stoch_adj_mat.R
@@ -0,0 +1,21 @@
+# Stochastic adjacency matrix used in the PageRank algorithm by Google
+
+stoch.adj.mat <- function(M){
+  if(nrow(M) == ncol(M)){
+    # by summing the outlinks we get the weight vector
+    get.weight.byrow <- function(x) {
+      x[x==1] <- 1/sum(x)
+      return(x)
+    }
+    M.adj <- t(apply(A,1,f))
+    M.eig <- eigen(M.adj)
+    return(list(stoch.matrix = M.adj, eigenvalues = M.eig$values, eigenvectors = M.eig$vectors)) 
+  } else {
+    print("Error: Not square matrix.")
+  }
+}
+
+
+A = matrix(c(0,0,1,0,0,1,1,0,1,1,0,0,0,0,0,1,1,1,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0),6,6,byrow = T)
+
+stoch.adj.mat(A)
